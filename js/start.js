d4ba871231db8477d2f93642b96d3d24ae7c008f
Tommy Tran <tommytrann@hotmail.com>
2016-05-10 11:33:44 -0700

    removed old tempColor match function, made a copy of new boostrap fonts

--- js/start.js
+++ js/start.js
@@ -1,54 +1,6 @@
 
 	
 
-function startTimer(){
-
-    var countdown =  $("#countdown").countdown360({
-        radius      : 90,
-        seconds     : 10,
-        fontColor   : '#FFFFFF',
-        autostart   : false,
-        onComplete : function() {window.loadInput()
-
-           // window.location = "input.html"
-
-        }
-        <!--onComplete  : function() { console.log('done') } -->
-    });
-    countdown.start();
-    console.log('countdown360 ',countdown);
-}
-
-function decideColor(numColors){
-    var shapeNum = Math.floor( Math.random() * numColors);
-    switch(shapeNum){
-        case 0:
-            return "colorRed";
-            break;
-        case 1:
-            return "colorBlue";
-            break;
-        default:
-            return "colorGreen";
-    }
-}
-function generateBoard(level){
-    var row,shape,styleShape,source,shapeClass;
-    for(i=0; i<level; i++) {
-        row = "shapelist"+decideRow();
-        shape = decideShape(decideNumShapes());
-        path = "images/"+shape+".jpg";
-        source="images/circle.jpg";
-        shapeClass = "block "+decideColor(decideNumColors())+" ui-draggable";
-
-        var img = $('<img />', {
-            id: shape,
-            src: path,
-            class: shapeClass
-        });
-        img.appendTo($("#"+row));
-    }
-}
 
 function decideShape(numShapes){
     var shapeNum = Math.floor( Math.random() * numShapes );
@@ -73,6 +25,32 @@ function decideColor(numColors){
     }
 }
 
+//button that checks the
+    $("#start_click").click(function () {
+        loadInput();
+        //level_shapes = getUserContent(0);
+        //localStorage.setItem("level_shapes",level_shapes.toString());
+        //
+        //window.location.href = "input.html";
+    });
+    $("#input_click").click(function () {
+        user_shapes = getUserContent(4);
+        //alert(document.cookie);
+
+        var level_shapes_str = localStorage.getItem("level_shapes");
+        console.log(user_shapes.toString());
+        if((user_shapes.toString()) == level_shapes_str){
+            //alert(level_shapes_str+"  mAtCh!"+user_shapes.toString());
+            level++;
+            localStorage.setItem("level",level);
+            console.log("level: "+level);
+            window.location.href = "start.html";
+        }
+        else {
+            var helpTip = localStorage.getItem("level shapes user");
+            alert(helpTip +"\n Sorry, no match, try inputing the above!");
+        }
+    });
 function generateBoard(level){
     var row,shape,styleShape,source,shapeClass;
     for(i=0; i<level; i++) {
