02986be6e2b273bf118310c32b3040eda5e1429c
Joshua Chan <jchan91913@gmail.com>
2016-05-10 11:18:14 -0700

    generating board function is repaired added libarary for touch

--- js/start.js
+++ js/start.js
@@ -58,6 +58,24 @@ function init(){
     sortList();
 }
 
+function generateBoard(level){
+    var row,shape,styleShape,source,shapeClass;
+    for(i=0; i<level; i++) {
+        row = "shapelist"+decideRow();
+        shape = decideShape(decideNumShapes());
+        path = "images/"+shape+".jpg";
+        source="images/circle.jpg";
+        shapeClass = "block "+decideColor(decideNumColors())+" ui-draggable";
+
+        var img = $('<img />', {
+            id: shape,
+            src: path,
+            class: shapeClass
+        });
+        img.appendTo($("#"+row));
+    }
+}
+
 function decideShape(numShapes){
     var shapeNum = Math.floor( Math.random() * numShapes );
     switch(shapeNum){
