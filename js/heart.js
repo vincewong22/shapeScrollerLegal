45939d4144856d82e7d734ef587fc35cf8ec7396
Tommy Tran <tommytrann@hotmail.com>
2016-05-12 19:29:53 -0700

    Revert "asd"
    This reverts commit ef53f26d1562770290e827b5a9974eecf66d79b1.

---
+++ js/heart.js
@@ -0,0 +1,16 @@
+draw 3 red hearts
+heart = 0; 
+
+function lifeSystem() {
+	for(i=0;i<=3;i++) {
+		if(inputArray != array) {
+			heart++;
+			drawblackhearts
+		}
+	}
+	
+	if(heart==3) {
+		reset to lvl 1
+		show score
+	}
+}
