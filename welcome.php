62a718e5089565f5c056e3c809a9069aceb7344c
vincewong22 <vincewong22@gmail.com>
2016-05-18 09:44:45 -0700

    added records to welcome

--- welcome.php
+++ welcome.php
@@ -15,15 +15,53 @@ $username = 'gofiuawo_vince';
 /*** mysql password ***/
 $password = '101010';
 
-try {
-    $dbh = new PDO("mysql:host=$hostname;dbname=gofiuawo_leaderboard", $username, $password);
-    /*** echo a message saying we have connected ***/
-    echo 'Connected to database';
+$dbname = "gofiuawo_leaderboard";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+ $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('" . $_POST['name'] . "', 'Doe', '" . $_POST['email'] . "');";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+} 
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
     }
-catch(PDOException $e)
-    {
-    echo $e->getMessage();
-    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
+
+
+
 </body>
 </html>
