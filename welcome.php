be8440343d3c7352d87e517fb4345bb25d2df894
Tommy Tran <tommytrann@hotmail.com>
2016-05-19 12:20:49 -0700

    FINISHED ish

--- welcome.php
+++ welcome.php
@@ -7,20 +7,22 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
 Your level achieved: <?php echo $_POST["level"]; ?><br>
+
+<h1>LEADERBOARD</h1>
 <?php
 
 /*** mysql hostname ***/
 $hostname = 'localhost';
 
 /*** mysql username ***/
-$username = 'gofiuawo_vince';
+$username = 'root';
 
 /*** mysql password ***/
-$password = '101010';
+$password = '';
 
 $dbname = "gofiuawo_leaderboard";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($hostname, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -62,27 +64,29 @@ $sql = "SELECT id, firstname, email,reg_date,level
 		"
 		;
 $result = $conn->query($sql);
+$rank =1;
 
 if ($result->num_rows > 0) {
     // output data of each row
 	echo'<div id="leaderboard">';
 	echo'<table>';
 	echo'<tr>';
-	echo'<th>Record #</th>';
+	echo'<th>Rank</th>';
 	echo'<th>Name</th>';
+	echo'<th>Level</th>';
 	echo'<th>Email</th>';
 	echo'<th>Date</th>';
-	echo'<th>Level Achieved</th>';
 	echo'</tr><br>';
     while($row = $result->fetch_assoc()) {
 		echo"<tr>";
         //echo "Record No:  " . $row["id"]. " - Name:  " . $row["firstname"]. "- Email:  " . $row["email"]."-Registration Date:  " . $row["reg_date"]. "<br>";
-		echo "<td>".$row["id"]."</td>";
+		echo "<td>"."{$rank}"."</td>";
 		echo "<td>".$row["firstname"]."</td>";
+		echo "<td>".$row["level"]."</td>";
 		echo "<td>".$row["email"]."</td>";
 		echo "<td>".$row["reg_date"]."</td>";
-		echo "<td>".$row["level"]."</td>";
 		echo"</tr>";
+		$rank++;
 	}
 	echo'</table>';
 	echo'</div>';
